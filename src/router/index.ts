ceea842ddcd75cc58878e470bddfde5607153f69
john <aarontu803@gmail.com>
2023-11-01 18:21:24 +0800

    fix:base-url

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
 
 const routes = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
